22b6ea84c67398a84400aa176cea682a49951c3c
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-01-24 12:53:10 +0900

    Add test for findfile.ExpandEnv

--- glob_test.go
+++ glob_test.go
@@ -1,8 +1,25 @@
 package findfile
 
 import (
+	"os"
 	"testing"
 )
+
+func TestExpandEnv(t *testing.T) {
+	temp := os.Getenv("TEMP")
+	if ExpandEnv("%TEMP%") != temp {
+		t.Fatal(`Fail: ExpandEnv("%TEMP%")`)
+		return
+	}
+	if ExpandEnv("$TEMP") != temp {
+		t.Fatal(`Fail: ExpandEnv("$TEMP")`)
+		return
+	}
+	if ExpandEnv("${TEMP}") != temp {
+		t.Fatal(`Fail: ExpandEnv("${TEMP}")`)
+		return
+	}
+}
 
 func TestGlob(t *testing.T) {
 	for _, pattern1 := range []string{"*", "", ".*"} {
