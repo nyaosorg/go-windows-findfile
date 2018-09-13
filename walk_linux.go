df2f3229db11eb0fb53a29bb7a838c84f930245e
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-09-14 01:57:03 +0900

    Support Linux partially

---
+++ walk_linux.go
@@ -0,0 +1,43 @@
+package findfile
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+type FileInfo struct {
+	os.FileInfo
+}
+
+func (fi *FileInfo) IsReadOnly() bool {
+	return (fi.Mode() & 0200) == 0
+}
+
+func (this *FileInfo) IsHidden() bool {
+	return false
+}
+
+func (fi *FileInfo) IsSystem() bool {
+	return false
+}
+
+func Walk(pattern string, callback func(*FileInfo) bool) error {
+	dir := filepath.Dir(pattern)
+	fnamepattern := filepath.Base(pattern)
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		match, err := filepath.Match(fnamepattern, file.Name())
+		if err != nil {
+			return err
+		}
+		if match && !callback(&FileInfo{file}) {
+			break
+		}
+	}
+	return nil
+}
