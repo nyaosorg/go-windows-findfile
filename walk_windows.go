e97d0b4e19b71a6b9de84793a8a29fcf9522d2fa
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-04-11 23:06:38 +0900

    Replace syscall -> golang.org/x/sys/windows

--- walk_windows.go
+++ walk_windows.go
@@ -4,14 +4,15 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"syscall"
 	"time"
+
+	"golang.org/x/sys/windows"
 )
 
 // FileInfo is the struct for Walk()'s parameter
 type FileInfo struct {
-	syscall.Win32finddata
-	handle syscall.Handle
+	windows.Win32finddata
+	handle windows.Handle
 }
 
 func (fi *FileInfo) clone() *FileInfo {
@@ -19,12 +20,12 @@ func (fi *FileInfo) clone() *FileInfo {
 }
 
 func findFirst(pattern string) (*FileInfo, error) {
-	pattern16, err := syscall.UTF16PtrFromString(pattern)
+	pattern16, err := windows.UTF16PtrFromString(pattern)
 	if err != nil {
 		return nil, err
 	}
 	this := new(FileInfo)
-	this.handle, err = syscall.FindFirstFile(pattern16, &this.Win32finddata)
+	this.handle, err = windows.FindFirstFile(pattern16, &this.Win32finddata)
 	if err != nil {
 		return nil, err
 	}
@@ -33,7 +34,7 @@ func findFirst(pattern string) (*FileInfo, error) {
 
 // Name returns file's name
 func (fi *FileInfo) Name() string {
-	return syscall.UTF16ToString(fi.FileName[:])
+	return windows.UTF16ToString(fi.FileName[:])
 }
 
 // Size returns file's size by bytes
@@ -64,11 +65,11 @@ func (fi *FileInfo) Sys() interface{} {
 }
 
 func (fi *FileInfo) findNext() error {
-	return syscall.FindNextFile(fi.handle, &fi.Win32finddata)
+	return windows.FindNextFile(fi.handle, &fi.Win32finddata)
 }
 
 func (fi *FileInfo) close() {
-	syscall.FindClose(fi.handle)
+	windows.FindClose(fi.handle)
 }
 
 // Attribute returns attributes-bit of the file.
@@ -78,27 +79,27 @@ func (fi *FileInfo) Attribute() uint32 {
 
 // IsReparsePoint returns true when the file has a reparse point attribute.
 func (fi *FileInfo) IsReparsePoint() bool {
-	return (fi.Attribute() & FILE_ATTRIBUTE_REPARSE_POINT) != 0
+	return (fi.Attribute() & windows.FILE_ATTRIBUTE_REPARSE_POINT) != 0
 }
 
 // IsReadOnly returns true when the file has a readonly attribute.
 func (fi *FileInfo) IsReadOnly() bool {
-	return (fi.Attribute() & syscall.FILE_ATTRIBUTE_READONLY) != 0
+	return (fi.Attribute() & windows.FILE_ATTRIBUTE_READONLY) != 0
 }
 
 // IsDir returns true when the file is directory.
 func (fi *FileInfo) IsDir() bool {
-	return (fi.Attribute() & syscall.FILE_ATTRIBUTE_DIRECTORY) != 0
+	return (fi.Attribute() & windows.FILE_ATTRIBUTE_DIRECTORY) != 0
 }
 
 // IsHidden returns true when the file is hidden.
 func (fi *FileInfo) IsHidden() bool {
-	return (fi.Attribute() & syscall.FILE_ATTRIBUTE_HIDDEN) != 0
+	return (fi.Attribute() & windows.FILE_ATTRIBUTE_HIDDEN) != 0
 }
 
 // IsSystem returns true when the file has a system attribute.
 func (fi *FileInfo) IsSystem() bool {
-	return (fi.Attribute() & syscall.FILE_ATTRIBUTE_SYSTEM) != 0
+	return (fi.Attribute() & windows.FILE_ATTRIBUTE_SYSTEM) != 0
 }
 
 // Walk enumerates the files matching patterns.
