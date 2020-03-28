eee69b678cbf26874d68a00c2148803a2774e883
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-03-29 01:21:19 +0900

    Rewrite testcode for GetFileAttributes

--- fileattr_test.go
+++ fileattr_test.go
@@ -1,12 +1,35 @@
 package findfile
 
 import (
+	"os"
 	"testing"
+
+	"golang.org/x/sys/windows"
 )
 
 func TestGetFileAttributes(t *testing.T) {
-	Walk("*", func(f *FileInfo) bool {
-		print(f.Name(), "...", GetFileAttributes(f), "\n")
-		return true
-	})
+	stat, err := os.Stat("fileattr_test.go")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	attr := GetFileAttributes(stat)
+	offbits := []uint32{
+		windows.FILE_ATTRIBUTE_READONLY,
+		windows.FILE_ATTRIBUTE_HIDDEN,
+		windows.FILE_ATTRIBUTE_SYSTEM,
+		windows.FILE_ATTRIBUTE_DIRECTORY,
+	}
+	for _, bit := range offbits {
+		if (attr & bit) != 0 {
+			t.Fatalf("normal file has invalid bit: %X", bit)
+		}
+	}
+	stat, err = os.Stat(".")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	attr = GetFileAttributes(stat)
+	if (attr & windows.FILE_ATTRIBUTE_DIRECTORY) == 0 {
+		t.Fatal("directory file does not have DIRECTORY-BIT")
+	}
 }
